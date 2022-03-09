6f74e190be5a63314b23f0469b0b1c0db1b21aae
sykovanz <tmfernandes002@gmail.com>
2022-03-09 00:14:35 +0000

    Added highlight to clock in current time

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -10,8 +11,13 @@ shuffle($grid_clocks_image_paths);
 $image_size = 84;
 
 echo "<style>
+
+body {
+    background-color: #1c1c1c;
+  }
 .grid-container {
     display: grid;
+    pointer-events: none;
 }
 
 @media (max-width: 1920px) {
@@ -38,12 +44,34 @@ echo "<style>
     }
 }
 
-
+.h".date("hi")."{
+    filter: contrast(160%);
+    filter: brightness(160%);
+}
 </style>";
 
+echo date("hi");
 echo "<div class='grid-container'>";
 foreach ($grid_clocks_image_paths as $gc_image){
     $image_path = ("$grid_clocks_folder" . "/" . "$gc_image");
-    echo "<img class='grid-clock' src='$image_path' style='width:$image_size'px';height:$image_size'px';'>";
+    $to_replace = array("hour", " ", "-128", ".png");
+    $hours_mins = str_replace($to_replace, "", $gc_image);
+    $hours_mins = explode("minute", $hours_mins);
+    $hours = sprintf("%02d",$hours_mins[0]);
+    $mins = sprintf("%02d",$hours_mins[1]);
+    $class_name = "h".$hours.$mins;
+    echo "<img class=$class_name src='$image_path' style='width:$image_size'px';height:$image_size'px';'>";
 }
 echo "</div>";
+?>
+<script>
+function printTest(){
+ $.ajax({
+    console.log(result.abc);
+ });
+}
+
+$(document).ready(function(){
+ setInterval(printTest,5000);
+});
+</script>
